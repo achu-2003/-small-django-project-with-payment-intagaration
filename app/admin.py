4861cdfb72e689ada526222694ea2e5f809815c3
Achuthan E <132340183+achu-2003@users.noreply.github.com>
2025-09-26 16:31:15 +0530

    good

--- app/admin.py
+++ app/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from .models import StudentInfo,Payment,StaffInfo,PendingStudent
 from import_export.admin import ImportExportModelAdmin
 
-
-
 class StaffInfoAdmin(admin.ModelAdmin):
     list_display=['name','email','mobile']
 
